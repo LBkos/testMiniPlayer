c55e285ff6473c46533817116a1a34d9c08e0309
LBkos <constantin.l.o.p@gmail.com>
2023-07-26 00:58:43 +0700

    Initial Commit

---
+++ MusicTest/MusicTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTestApp.swift
+//  MusicTest
+//
+//  Created by Константин Лопаткин on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
